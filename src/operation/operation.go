47a7d8b73426325fb9362ffa4e0b820b4dfc2ad6
Nikita <nekotsokolov2001@gmail.com>
2026-02-23 12:23:34 +0300

    Update operation. New calculation algorithm

--- src/operation/operation.go
+++ src/operation/operation.go
@@ -1,55 +1,34 @@
 package operation
 
-import (
-	"readfile"
-)
-
 // Average - вычисляет среднее значение из тектового файла
-func Average(fPath string) (average float64, err error) {
-	// Считывает значение из текстового файла
-	array, err := readfile.ReadFile(fPath)
-	// Проверка на ошибки
+func Average(data []float64) (average float64, err error) {
+	// Суммирует полученные значения из файла функции Sum
+	sum, err := Sum(data)
+	// Проверка на ошибки при расчете
 	if err != nil {
-		return average, err
+		return sum, err
 	}
-	// Суммирует полученные значения из файла функцией Sum
-	sum, err := Sum(fPath)
-	if err != nil {
-		return average, err
-	}
-	// Считывает длину массива
-	arrLen := float64(len(array))
-	// Находит среднее значение
-	average = sum / arrLen
-	// Возвращает полученное среднее значение
+	// Считает длину массива
+	lenArr := float64(len(data))
+	// Считает среднее
+	average = sum / lenArr
 	return average, nil
 }
 
 // Sum - суммирует значения из текстового файла
-func Sum(fpath string) (sum float64, err error) {
-	// Считывает значения из файла
-	array, err := readfile.ReadFile(fpath)
-	if err != nil {
-		return sum, err
-	}
+func Sum(data []float64) (sum float64, err error) {
 	// Через for ... range производит суммирование каждой строчки файла
-	for i, _ := range array {
-		sum += array[i]
+	for i, _ := range data {
+		sum += data[i]
 	}
 	// Возвращает полученное значение
 	return sum, nil
 }
 
 // Diff - находи разность между значениями из файла
-func Diff(fpath string) (diff float64, err error) {
-	// Считывает значения из файла
-	array, err := readfile.ReadFile(fpath)
-	// Проверка на ошибки
-	if err != nil {
-		return diff, err
-	}
-	for i, _ := range array {
-		diff -= array[i]
+func Diff(data []float64) (diff float64, err error) {
+	for i, _ := range data {
+		diff -= data[i]
 	}
 	// Возвращает полученное значение
 	return diff, err
