20b20c81d2240a340a8748f1837fa19524cb2e43
Nikita <nekotsokolov2001@gmail.com>
2026-02-23 13:06:11 +0300

    Stable version 23/02/2026

--- src/main/main.go
+++ src/main/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"operation"
+	"readfile"
+)
 
+func main() {
+	data, err := readfile.ReadFile("text.txt")
+	if err != nil {
+		fmt.Println("Ошибка!", err)
+	}
+	res, err := operation.Sum(data)
+	if err != nil {
+		fmt.Println("Ошибка!", err)
+	}
+	fmt.Println("Результат расчета:", res)
 }
